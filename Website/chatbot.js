fe2a539af61b1728200886d5c224984f29453aee
jsimon90 <jsimon90@uw.edu>
2023-10-14 19:35:58 -0700

    fixed

--- Website/chatbot.js
+++ Website/chatbot.js
@@ -1,3 +1,4 @@
+prevMessage = "";
 document.addEventListener("DOMContentLoaded", function () {
     const inputForm = document.getElementById("input-form");
     const inputField = document.getElementById("input-field");
@@ -7,45 +8,39 @@ document.addEventListener("DOMContentLoaded", function () {
         e.preventDefault(); // Prevent the form from submitting and refreshing the page
 
         const userMessage = inputField.value;
-        
 
         // Display the user's message in the chat
         addMessage("user", userMessage);
 
         // Here, you can process the user's message and provide a response from the chatbot
         // For simplicity, we'll add a basic response.
-        setTimeout(function () {
-            const chatbotResponse = "Thanks for your message! How can I assist you?";
-            addMessage("chatbot", chatbotResponse);
-
-        }, 100);
+        const chatbotResponse = "Thanks for your message! How can I assist you?";
+        addMessage("chatbot", chatbotResponse);
 
         // Clear the input field
         inputField.value = "";
 
-        scrollToBottom();
+        scrollToBottom(); // Scroll to the bottom when a new message is added
     });
-    function openChatbox() {
-        // Redirect to a blank page when the circle is clicked
-        window.location.href = "https://www.google.com/?client=safari";
-    }
-    
-    
-    
+
     function scrollToBottom() {
-        messages.scrollTop = messages.scrollHeight;
-      }
-      
+        const lastMessage = conversation.lastElementChild;
+        lastMessage.scrollIntoView({ behavior: "smooth" });
+    }
 
     function addMessage(sender, message) {
+        if (message === '' || message === prevMessage) {
+            return;
+        }
         const messageContainer = document.createElement("div");
-        messageContainer.classList.add("chatbot-message");
-
         const messageText = document.createElement("p");
         messageText.classList.add(sender === "user" ? "user-text" : "chatbot-text");
         messageText.textContent = message;
 
         messageContainer.appendChild(messageText);
         conversation.appendChild(messageContainer);
+        prevMessage = message;
+
+        scrollToBottom(); // Scroll to the bottom when a new message is added
     }
 });
